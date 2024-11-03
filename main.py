6ff3019253e01acedb710ee07e1af5d745db76a5
viggohm <viggo@holtens.dk>
2024-11-03 14:22:44 +0100

    adding report and sort functionality

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ def main():
     text = get_text(book_path)
     word_count = count_words(text)
     letter_count = count_letters(text)
-    print(word_count)
-    print(letter_count) 
+    list_of_letter_count = convert_dict_to_list_of_dict(letter_count)
+    report(book_path, word_count, list_of_letter_count)
+
 
 def get_text(path):
     with open(path) as f:
@@ -23,5 +24,25 @@ def count_letters(book):
         else:
             letter_count[letter] = 1
     return letter_count
+
+def convert_dict_to_list_of_dict(letter_count):
+    list_of_letter_count = []
+    for letter in letter_count:
+        if letter.isalpha():
+            list_of_letter_count.append({"letter": letter, "count": letter_count[letter]})
+    
+    def sort_on(dict):
+        return dict["count"]
+    list_of_letter_count.sort(reverse=True, key=sort_on)
+
+    return list_of_letter_count
+
+def report(book_path, word_count, list_of_letter_count):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+    for letter_count in list_of_letter_count:
+        print(f"The '{letter_count["letter"]}' character was found {letter_count["count"]} times")
+    print("--- End report ---")
+
 
 main()
