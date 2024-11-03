238648b0e5b41bbf99d3686e0bf743bfab8f86fe
viggohm <viggo@holtens.dk>
2024-11-03 13:35:18 +0100

    adding letter count

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     word_count = count_words(text)
+    letter_count = count_letters(text)
     print(word_count)
+    print(letter_count) 
 
 def get_text(path):
     with open(path) as f:
@@ -11,5 +13,15 @@ def get_text(path):
 def count_words(book):
     words = book.split()
     return len(words)
+
+def count_letters(book):
+    letter_count = {}
+    book = book.lower()
+    for letter in book:
+        if letter in letter_count:
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+    return letter_count
 
 main()
